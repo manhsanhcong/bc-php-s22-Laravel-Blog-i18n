1fc265f4c5506d5aa0542149810d273b8bb8ec12
manhsanhcong <manhsanhcong.08@gmail.com>
2018-11-30 14:54:35 +0700

    add file

---
+++ resources/lang/en/language.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hoanltm
+ * Date: 30/11/2018
+ * Time: 14:44
+ */
+return [
+    'blog_application' => 'Blog Application',
+    'home' => 'Home',
+    'list_post' => 'Post list',
+    'create_post' => 'Create Post',
+    'save' => 'Save',
+    'post_not_exist' => 'Post not exist',
+    'title' => 'Title',
+    'content' => 'Content',
+];
---
+++ resources/lang/vi/language.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hoanltm
+ * Date: 30/11/2018
+ * Time: 14:44
+ */
+return [
+    'blog_application' => 'Ứng dụng Blog',
+    'home' => 'Trang chủ',
+    'list_post' => 'Danh Sách Bài Viết',
+    'create_post' => 'Tạo Bài Viết',
+    'save' => 'Lưu',
+    ];
